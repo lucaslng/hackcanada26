5bc878d48d7b5c518b40ce35e541dba20050154e
ryannchng <ryanteddy1716@gmail.com>
2026-03-07 14:26:48 -0500

    add canadian flag to bottom left corner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,12 @@
 // Footer.tsx
 
-import { MapleLeaf } from './MapleLeaf';
+import canadianFlag from '../../assets/canadian_flag.png';
 
 export function Footer() {
   return (
     <footer className="site-footer">
       <div className="footer-inner">
-        <MapleLeaf />
+        <img src={canadianFlag} alt="" className="footer-flag" />
         <span>© Government of Canada · Service Canada Document Portal Demo</span>
         <span className="footer-disclaimer">
           This is a demonstration application. Not an official Government of Canada service.
